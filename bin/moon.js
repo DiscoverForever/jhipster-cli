f9e54a4ceed38ddd9303030332b71c8272f8ad6d
tangyy <tangyueyang@lztogether.com>
2017-10-19 20:57:25 +0800

    Add moon.js

---
+++ bin/moon.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+require('commander')
+.version(require('../package').version)
+.usage('<command> [options]')
+.command('init', 'generate a new project from config')
+.command('export', 'export database as markdown or excel from leancloud')
+
+.parse(process.argv)
