6857b626e8b224fbf88724a420bbba9cbbf9fadc
tangyy <tangyueyang@lztogether.com>
2017-10-20 15:37:40 +0800

    Mod 代码优化

--- bin/moon-init.js
+++ bin/moon-init.js
@@ -3,54 +3,50 @@ const core = require('jhipster-core');
 const parse = core.parse;
 const path = require('path');
 const logger = require('../lib/logger');
+const utils = require('../lib/utils');
+const program = require('commander');
+const CWD = process.cwd();
+program
+  .usage('<path>')
+  .option('-p, --jdlpath <string>', 'jdl file or jdl dir path')
+  .parse(process.argv)
 
-/**
- * 读取JDL文件
- * @param targetPath JDL文件路径
- * @returns { JSON }
- */
-function readJDLFile(targetPath) {
-  const data = fs.readFileSync(targetPath).toString();
-  return parse(data);
-}
-
-/**
- * 读取文件目录
- * @param targetPath 
- * @returns 
- */
-function readJDLDir(targetPath) {
-  return fs.readdirSync(targetPath);
-}
 
 /**
  * 生成实体
  * @param jdl 
  */
-function generateEntities (jdl) {
+function generateEntities(jdl) {
   const entityTpl = require('../template/entity');
   const hookTpl = require('../template/hook');
   jdl.entities.forEach(entity => {
     const entityTemplate = entityTpl.generateEntity(entity);
     const hookTemplate = hookTpl.generateHook(entity);
-    logger.info('generate file', path.join(__dirname, '../entities', `${entity.name}.g.ts`));
-    logger.info('generate file', path.join(__dirname, '../hook', `${entity.name}.hook.g.js`));
-    fs.writeFileSync(path.join(process.cwd(), '/entities/', `${entity.name}.g.ts`), entityTemplate);
-    fs.writeFileSync(path.join(process.cwd(), '/hook/', `${entity.name}.hook.g.js`), hookTemplate);
+    logger.info('generate file', path.join(CWD, '../entities', `${entity.name}.g.ts`));
+    logger.info('generate file', path.join(CWD, '../hook', `${entity.name}.hook.g.js`));
+    fs.writeFileSync(path.join(CWD, '/entities/', `${entity.name}.g.ts`), entityTemplate);
+    fs.writeFileSync(path.join(CWD, '/hook/', `${entity.name}.hook.g.js`), hookTemplate);
   });
 }
 
 /**
  * 入口函数
  */
-function main() {
-  if (!fs.existsSync(path.join(process.cwd(), '/entities'))) fs.mkdirSync(path.join(process.cwd(), '/entities'));
-  if (!fs.existsSync(path.join(process.cwd(), '/hook'))) fs.mkdirSync(path.join(process.cwd(), '/hook'));
-  let dirs = readJDLDir(path.join(__dirname, '../dsl'));
-  dirs.map(fileName => {
-    let jdl = readJDLFile(path.join(__dirname, '../dsl', fileName));
+function main(jdlpath) {
+  if (!fs.existsSync(path.join(CWD, '/entities'))) fs.mkdirSync(path.join(CWD, '/entities'));
+  if (!fs.existsSync(path.join(CWD, '/hook'))) fs.mkdirSync(path.join(CWD, '/hook'));
+  if (fs.statSync(jdlpath).isDirectory()) {
+    let dirs = utils.readJDLDir(path.join(CWD, jdlpath));
+    dirs.map(fileName => {
+      let jdl = utils.readJDLFile(path.join(CWD, jdlpath, fileName));
+      generateEntities(jdl);
+    });
+  }
+  if (fs.statSync(jdlpath).isFile()) {
+    let jdl = utils.readJDLFile(path.join(CWD, jdlpath));
     generateEntities(jdl);
-  });
+  }
+  
 }
 
-main();
+if (program.jdlpath) main(program.jdlpath);
