82e17da0f8f7e6376ad682c012a4a057b826ed8d
tangyy <tangyueyang@lztogether.com>
2017-11-01 18:06:02 +0800

    Add ora adn gen leanengine

--- bin/moon-init.js
+++ bin/moon-init.js
@@ -5,7 +5,9 @@ const path = require('path');
 const logger = require('../lib/logger');
 const utils = require('../lib/utils');
 const program = require('commander');
+const download = require('download-git-repo');
 const CWD = process.cwd();
+const ora = require('ora')
 program
   .usage('<path>')
   .option('-p, --jdlpath <string>', 'jdl file or jdl dir path')
@@ -30,22 +32,34 @@ function generateEntities(jdl) {
 }
 
 /**
+ * 下载并生成leancloud云引擎
+ */
+function downloadAndGenerateLeanengine() {
+    download('https://github.com/leancloud/node-js-getting-started', 'test/tmp', { clone: true }, function (err) {
+        console.log(err)
+    })
+}
+
+/**
  * 入口函数
  */
 function main(jdlpath) {
-  if (!fs.existsSync(path.join(CWD, '/entities'))) fs.mkdirSync(path.join(CWD, '/entities'));
-  if (!fs.existsSync(path.join(CWD, '/hook'))) fs.mkdirSync(path.join(CWD, '/hook'));
-  if (fs.statSync(jdlpath).isDirectory()) {
-    let dirs = utils.readJDLDir(path.join(CWD, jdlpath));
-    dirs.map(fileName => {
-      let jdl = utils.readJDLFile(path.join(CWD, jdlpath, fileName));
-      generateEntities(jdl);
-    });
-  }
-  if (fs.statSync(jdlpath).isFile()) {
-    let jdl = utils.readJDLFile(path.join(CWD, jdlpath));
-    generateEntities(jdl);
-  }
+    const spinner = ora('download template').start();
+    spinner.start();
+    // downloadAndGenerateLeanengine();
+  // if (!fs.existsSync(path.join(CWD, '/entities'))) fs.mkdirSync(path.join(CWD, '/entities'));
+  // if (!fs.existsSync(path.join(CWD, '/hook'))) fs.mkdirSync(path.join(CWD, '/hook'));
+  // if (fs.statSync(jdlpath).isDirectory()) {
+  //   let dirs = utils.readJDLDir(path.join(CWD, jdlpath));
+  //   dirs.map(fileName => {
+  //     let jdl = utils.readJDLFile(path.join(CWD, jdlpath, fileName));
+  //     generateEntities(jdl);
+  //   });
+  // }
+  // if (fs.statSync(jdlpath).isFile()) {
+  //   let jdl = utils.readJDLFile(path.join(CWD, jdlpath));
+  //   generateEntities(jdl);
+  // }
   
 }
 
