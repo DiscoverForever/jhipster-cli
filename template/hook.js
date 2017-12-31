1e81717f8ff5353d95ecd3fd644f84da2879c61c
tangyy <tangyueyang@lztogether.com>
2017-12-31 18:17:40 +0800

    Mod 状态机done

--- template/hook.js
+++ template/hook.js
@@ -1,22 +1,27 @@
-
-
 const AV = require('leanengine');
 /** 保存前 */
-AV.Cloud.beforeSave('<%=entity.name%>', function(request) {});
+AV.Cloud.beforeSave('<%=entities[0].name%>', function(request) {});
 /** 保存后 */
-AV.Cloud.afterSave('<%=entity.name%>', function(request) {});
+AV.Cloud.afterSave('<%=entities[0].name%>', function(request) {});
 /** 更新前 */
-AV.Cloud.beforeUpdate('<%=entity.name%>', function(request) {});
+AV.Cloud.beforeUpdate('<%=entities[0].name%>', function(request) {});
 /** 更新后 */
-AV.Cloud.afterUpdate('<%=entity.name%>', function(request) {});
+AV.Cloud.afterUpdate('<%=entities[0].name%>', function(request) {});
 /** 删除前 */
-AV.Cloud.beforeDelete('<%=entity.name%>', function(request) {});
+AV.Cloud.beforeDelete('<%=entities[0].name%>', function(request) {});
 /** 删除后 */
-AV.Cloud.afterDelete('<%=entity.name%>', function(request) {});
-<% entity.body.forEach(prop => { %>
-  <% if (prop.type === 'Service') { %>
+AV.Cloud.afterDelete('<%=entities[0].name%>', function(request) {});
+<% entities[1]&&entities[1].body.forEach(prop => { %>
 /**<%=prop.javadoc%>*/
-AV.Cloud.define('<%=prop.name%>',  function(request){});
-  <%}%>
+AV.Cloud.define('<%=prop.name%>', async ({params, currentUser, sessionToken, meta}) => {
+  const objectId = params.objectId;
+  if (!objectId) throw new Error('objectId must not null');
+  const entity = AV.Object.createWithoutData('<%=entities[0].name%>', objectId);
+  await entity.fetch();
+  let currentState = entity.get('state');
+  if (currentState !== <%=state.values.indexOf(prop.type.split('__')[0])%>) throw new Error('currentState must be <%=state.values.indexOf(prop.type.split('__')[0])%>,but get ${currentState}');
+  entity.set('state', <%=state.values.indexOf(prop.type.split('__')[1])%>);
+  return entity.save(null, {sessionToken});
+});
 <%})%>
   
