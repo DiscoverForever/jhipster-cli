e0e4891efc78671bc8f5f6184cba803cc8673e24
tangyy <tangyueyang@lztogether.com>
2018-01-02 18:29:20 +0800

    Mod beforeSave迁移到*.g

--- template/hook.js
+++ template/hook.js
@@ -5,24 +5,6 @@ const G_<%=entities[0].name%> = require('./<%=entities[0].name%>.g');
 AV.Cloud.beforeSave('<%=entities[0].name%>', async function(request) {
   await EX_<%=entities[0].name%>.<%=entities[0].name%>_beforeSave(request);
   return G_<%=entities[0].name%>.<%=entities[0].name%>_beforeSave(request);
-  function setACL() {
-    let entity = request.object;
-    let acl = new AV.ACL();
-    const ACL = <%-JSON.stringify(ACL).replace(/^"(.*)"$/g, '$1')%>;
-    const ROLE_<%=entities[0].name%> = new AV.Role();
-    for (var key in ACL) {
-      if (key === '*') {
-        acl.setPublicReadAccess(ACL[key][read]);
-        acl.setPublicWriteAccess(ACL[key][write]);
-      }
-      if (key.startsWith('role')) {
-        const roleName = key.split(':')[1];
-        acl.setRoleReadAccess(roleName, ACL[key][read])
-        acl.setPublicWriteAccess(roleName, ACL[key][write]);
-      }
-    }
-    entity.setACL(acl);
-  }
 });
 /** 保存后 */
 AV.Cloud.afterSave('<%=entities[0].name%>', async function(request) {
